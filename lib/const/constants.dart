d92a8d6b7d2db4c2f6f5122c1212003fcadb8a73
Mahdiyar Merrixi <mahdiyarmerrikhi2008@gmail.com>
2025-03-29 23:28:47 +0330

    constance added

---
+++ lib/const/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Constance {
+  static Color primaryColor = Colors.green;
+  static Color blackColor = Colors.black;
+
+  static String titleOne = 'گیاهان را بهتر از قبل درک کن';
+  static String descriptionOne =
+      'در مورد نگهداری گل و گیاهان میتوانی اطلاعات کسب کنی';
+  static String titleTwo = 'با گیاهان جدید اشنا شو';
+  static String descriptionTwo =
+      'رز مشکی یا گل رز دوست داری؟ میتونی اینجا پیداشون کنی';
+  static String titlethree = 'با یک گل بهار نمیشود ,گل بکار';
+  static String descriptionthree =
+      'هر گلی نیاز داشته باشید تو این اپلیکیشن میتونید پیدا کنید';
+}
